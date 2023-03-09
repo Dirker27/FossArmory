0fb88e176db286111e55d1eeea5a49d73708a334
Dirk <hortensd@gmail.com>
2023-03-09 01:43:47 -0800

    Initial Scene Draft with Sample Mole Shooter.

---
+++ Assets/SampleMole/GunnerRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunnerRotation : MonoBehaviour {
+
+    public float rotationSpeed = 1.0f;
+    public CameraAim targetingCamera;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (targetingCamera)
+		{
+			Ray target = targetingCamera.GetAimingRay();
+			Quaternion lookAt = Quaternion.LookRotation(target.direction, Vector3.up);
+
+			transform.rotation = Quaternion.Lerp(transform.rotation, lookAt,
+				0.1f * rotationSpeed * Time.deltaTime);
+		}
+	}
+}
